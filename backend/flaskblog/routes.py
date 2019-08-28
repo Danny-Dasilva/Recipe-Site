98a884b6facd3205737ceeb259ceb165eeccc28a
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-08-28 06:41:33 -0400

    rewrote login and register routes

--- backend/flaskblog/routes.py
+++ backend/flaskblog/routes.py
@@ -268,3 +268,34 @@ def test():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register1", methods=['GET', 'POST'])
+def register1():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('test-reg.html', title='Register', form=form)
+
+
+@app.route("/login1", methods=['GET', 'POST'])
+def login1():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('test-log.html', title='Login', form=form)
